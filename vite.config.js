ce4830d2f21f293337e1ed8600af9c9d5462c042
Minu M Das <minumdas@gmail.com>
2025-10-05 22:42:41 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Exam-1/",  // ← critical for GitHub Pages
   plugins: [react()],
-})
+});
